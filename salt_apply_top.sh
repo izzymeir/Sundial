122efdb2557e508edca751845698b046382721ee
Yisroel Meir Blumstein <izzymeir@gmail.com>
2016-12-15 11:41:29 +0200

    Directory issues
    make sure the /srv/salt and pillar directories are there

--- salt_apply_top.sh
+++ salt_apply_top.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-# Copy the salt and pillar files to their correct directories, and tell Salt to apply the highstate in top.sls
+
+#Make sure that the salt and pillar directories are there
+sudo mkdir /srv/salt
+sudo mkdir /srv/pillar
+
+# Copy the salt and pillar files to their correct directories
 sudo cp -r salt/* /srv/salt
 sudo cp -r pillar/* /srv/pillar
+
+# Tell Salt to apply the highstate in top.sls
 sudo salt-call --local state.apply
+
 echo "DONE\n"
