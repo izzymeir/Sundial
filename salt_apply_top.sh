acba7f3b9979eccf3c9f116b5bf7f14bf43b3b47
Yisroel Meir Blumstein <izzymeir@gmail.com>
2017-01-17 10:34:04 +0200

    Fix script to apply salt high-state

--- salt_apply_top.sh
+++ salt_apply_top.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-#Make sure that the salt and pillar directories are there
-sudo mkdir /srv/salt
-sudo mkdir /srv/pillar
-
 # Copy the salt and pillar files to their correct directories
-sudo cp -r salt/* /srv/salt
-sudo cp -r pillar/* /srv/pillar
+sudo cp -r salt /srv
+sudo cp -r pillar /srv
 
 # Tell Salt to apply the highstate in top.sls
 sudo salt-call --local state.apply
